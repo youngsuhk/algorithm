84dd83202e9ba1dbbe4f8024e1d5c70677ae2b84
youngsuk <33721972+youngsuk0304@users.noreply.github.com>
2021-04-24 16:05:21 +0900

    BruteForce

---
+++ baekjoon_java/src/baekjoon_java/StepByStep/BruteForce/BruteForce.java
@@ -0,0 +1,5 @@
+package baekjoon_java.StepByStep.BruteForce;
+
+public class BruteForce {
+
+}
--- baekjoon_java/src/baekjoon_java/StepByStep/StepByStep.java
+++ baekjoon_java/src/baekjoon_java/StepByStep/StepByStep.java
@@ -2,9 +2,4 @@ package baekjoon_java.StepByStep;
 
 public class StepByStep {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
 }
