7a4486ecf81c26cd97d5ccacca751a16f7aadfaa
youngsuk <33721972+youngsuk0304@users.noreply.github.com>
2021-05-05 00:01:30 +0900

    Network
    union-find를 사용
    public static int find(int x) {
    	if (root[x] == x) {
    		return x;
    	} else {
    		return root[x] = find(root[x]);
    	}
    }
    
    public static void union(int x, int y) {
    	x = find(x);
    	y = find(y);
    
    	// 두 값의 root가 같으면(이미 같은 트리) 합치지 않는다.
    	// "union-by-rank 최적화"
    	// 항상 높이가 더 낮은 트리를 높이가 높은 트리 밑에 넣는다. 즉, 높이가 더 높은 쪽을 root로 삼음
    	if (rank[x] < rank[y]) {
    		root[x] = y; // x의 root를 y로 변경
    	} else {
    		root[y] = x; // y의 root를 x로 변경
    		if (rank[x] == rank[y])
    			rank[x]++; // 만약 높이가 같다면 합친 후 (x의 높이 + 1)
    	}
    }
    이문제에서 주의할 점 !!
    union의결과가 같은 union 집합이어도 뒷부분의 변경으로 인해 root가 다르게
    나올 수 있다. 그래서 마지막에 root를 새롭게 찾아 줘야한다.
    dfs로 풀이 추가

--- ProgrammersJava/ProgrammersJava/src/BFS_DFS/Network_Dfs.java
+++ ProgrammersJava/ProgrammersJava/src/BFS_DFS/Network_Dfs.java
@@ -15,23 +15,25 @@ public class Network_Dfs {
 		int[][] computers2 = { { 1, 0, 0, 1 }, { 0, 1, 0, 0 }, { 0, 0, 1, 0 }, { 1, 0, 0, 1 } };
 		System.out.println(solution(n, computers));
 	}
+
 	public static int solution(int n, int[][] computers) {
-        int answer = 0;
-        boolean[] chk = new boolean[n];
-        for(int i = 0; i < n; i++) {
-            if(!chk[i]) {
-                dfs(computers, chk, i);
-                answer++;
-            }
-        }
-        return answer;
-    }
-    public static void dfs(int[][] computers, boolean[] chk, int start) {
-        chk[start] = true;
-        for(int i = 0; i < computers.length; i++) {
-            if(computers[start][i] == 1 && !chk[i]) {
-                dfs(computers, chk, i);
-            }
-        }
-    }
+		int answer = 0;
+		boolean[] chk = new boolean[n];
+		for (int i = 0; i < n; i++) {
+			if (!chk[i]) {
+				dfs(computers, chk, i);
+				answer++;
+			}
+		}
+		return answer;
+	}
+
+	public static void dfs(int[][] computers, boolean[] chk, int start) {
+		chk[start] = true;
+		for (int i = 0; i < computers.length; i++) {
+			if (computers[start][i] == 1 && !chk[i]) {
+				dfs(computers, chk, i);
+			}
+		}
+	}
 }
