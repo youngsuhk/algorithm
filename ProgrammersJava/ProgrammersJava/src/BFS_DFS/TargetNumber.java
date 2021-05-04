631e3779ad4b31226f5a5eda65fee5ef5ce084a2
youngsuk <33721972+youngsuk0304@users.noreply.github.com>
2021-05-04 20:16:04 +0900

    TargetNumber
    public static int dfs(int[] numbers, int target, int sum, int i) {
    	//종료조건
    	if(i == numbers.length) {//numbers의 마지막 자리
            	if(sum == target) {//더한값이 target과 같은지 확인
            	return 1;//같을경우 개수 추가
    		} else {
            		return 0;
            	}
            }
    
            int result = 0;
    	//다음 숫자 더하기
            result += dfs(numbers, target, sum+numbers[i], i+1);
            //다음 숫자 빼기
    	result += dfs(numbers, target, sum-numbers[i], i+1);
            return result;
    }
    
    void DFS(int index){
    	check_dfs[index] = true; //정점 방문 체크
    	printf("%d ",index); //방문한 정점 출력
    
    	for(int i=0;i<v[index].size();i++){
        		int next = v[index][i];
    
    		if(!check_dfs[next]){ //방문하지 않았다면 DFS() 호출
    			DFS(next);
    		}
    	}
    }

---
+++ ProgrammersJava/ProgrammersJava/src/BFS_DFS/TargetNumber.java
@@ -0,0 +1,32 @@
+package BFS_DFS;
+
+public class TargetNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int []number = {1,1,1,1,1};
+		int target=3;
+		System.out.println(solution(number,target));
+	}
+	 public static int solution(int[] numbers, int target) {
+	        int answer = 0;
+	        answer = dfs(numbers, target, numbers[0], 1) + dfs(numbers, target, -numbers[0], 1);
+
+	        return answer;
+	 }
+	 public static int dfs(int[] numbers, int target, int sum, int i) {
+	        
+	        if(i == numbers.length) {
+	            if(sum == target) {
+	                return 1;
+	            } else {
+	                return 0;
+	            }
+	        }
+	        
+	        int result = 0;
+	        result += dfs(numbers, target, sum+numbers[i], i+1);
+	        result += dfs(numbers, target, sum-numbers[i], i+1);
+	        return result;
+	    }
+}
