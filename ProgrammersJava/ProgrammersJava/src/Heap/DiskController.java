62cb8378331db9788e9f807daa6ddcc849918fe5
youngsuk <33721972+youngsuk0304@users.noreply.github.com>
2021-05-23 17:32:45 +0900

    DiskControll

---
+++ ProgrammersJava/ProgrammersJava/src/Heap/DiskController.java
@@ -0,0 +1,72 @@
+package Heap;
+
+import java.util.*;
+
+public class DiskController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] jobs = { { 2, 4 }, { 2, 3 }, { 3, 10 } };
+		//System.out.println(jobs[0][0]);
+		System.out.println(solution(jobs));
+	}
+
+	static class Job implements Comparable<Job>{
+		int requestTime;
+		int workingTime;
+
+		public Job(int requestTime, int workingTime) {
+			this.requestTime = requestTime;
+			this.workingTime = workingTime;
+		}
+		@Override
+	    public int compareTo(Job n)
+	    {
+	        if(n.requestTime != requestTime)
+	            if(n.requestTime < requestTime)
+	                return 1;
+	            else
+	                return -1;
+	        else
+	            if(n.workingTime < workingTime)
+	                return 1;
+	            else
+	                return -1;
+	    }	    
+	}
+
+	public static int solution(int[][] jobs) {
+		int answer = 0;
+		int cnt = 0;
+		LinkedList<Job> list = new LinkedList<>();
+		PriorityQueue<Job> pq = new PriorityQueue<>(new Comparator<Job>() {
+    		@Override
+    		public int compare(Job j1, Job j2) {
+    			return j1.workingTime - j2.workingTime;
+    		}
+    	});
+		for (int i = 0; i < jobs.length; i++) {
+			list.add(new Job(jobs[i][0],jobs[i][1]));
+		}
+		Collections.sort(list);
+		
+		
+    	int time = list.peek().requestTime;
+
+    	while(cnt < jobs.length) {
+    		while(!list.isEmpty() && list.peek().requestTime <= time) {
+    			pq.offer(list.pollFirst());
+    		}
+    		
+    		if(!pq.isEmpty()) {
+    			Job job = pq.poll();
+    			time += job.workingTime;
+    			answer += time - job.requestTime;
+    			cnt++;
+    		} else {
+    			time++;
+    		}
+    	}
+    	return answer / cnt;
+    }
+}
--- ProgrammersJava/ProgrammersJava/src/Heap/MoreSpicy.java
+++ ProgrammersJava/ProgrammersJava/src/Heap/MoreSpicy.java
@@ -2,6 +2,7 @@ package Heap;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 //섞은 음식의 스코빌 지수 = 가장 맵지 않은 음식의 스코빌 지수 + (두 번째로 맵지 않은 음식의 스코빌 지수 * 2)
 
@@ -15,6 +16,7 @@ public class MoreSpicy {
 		System.out.println(solution(scoville, K));
 	}
 
+	// 단순 배열을 이용한 코드 : 시간 초가
 //	public static int solution(int[] scoville, long K) {
 //        int answer = 0;
 //        int temp=0;
@@ -40,39 +42,61 @@ public class MoreSpicy {
 //        }
 //        
 //    }
-	public static int solution(int[] scoville, long K) {
-		int answer = 0;
-		ArrayList<Integer> arr = new ArrayList<Integer>();
-		Arrays.sort(scoville);
-		// list로 변경
-		for (int i = 0; i < scoville.length; i++) {
-			arr.add(scoville[i]);
-		}
-		while (true) {
-			arr.sort(null);
-			// 하나만 존재하는데 K보다 작으면 -1리턴
-			if (arr.size() == 1) {
-				if (arr.get(0) < K)
-					return -1;
-			}
-			// 하나만 존재하는데 K보다 크면 answer리턴
-			else {
-				if (arr.get(0) > K)
-					return answer;
-			}
+	// ArrayList를 이용한 코드 : 효율성 테스트에서 시간 초가
+//	public static int solution(int[] scoville, long K) {
+//		int answer = 0;
+//		ArrayList<Integer> arr = new ArrayList<Integer>();
+//		Arrays.sort(scoville);
+//		// list로 변경
+//		for (int i = 0; i < scoville.length; i++) {
+//			arr.add(scoville[i]);
+//		}
+//		while (true) {
+//			arr.sort(null);
+//			// 하나만 존재하는데 K보다 작으면 -1리턴
+//			if (arr.size() == 1) {
+//				if (arr.get(0) < K)
+//					return -1;
+//			}
+//			// 하나만 존재하는데 K보다 크면 answer리턴
+//			else {
+//				if (arr.get(0) > K)
+//					return answer;
+//			}
+//
+//			// 2개 이상 존재할 경우 둘중 하나가 K보다 작을 경우
+//			if (arr.get(0) < K) {
+//
+//				arr.add(0, (arr.remove(0) + (arr.remove(0) * 2)));
+//				for (int i = 0; i < arr.size(); i++) {
+//					System.out.print(arr.get(i) + " ");
+//				}
+//				System.out.println("size : " + arr.size());
+//
+//				answer++;
+//			} else
+//				return answer;
+//		}
+//	}
+	//PriorityQueue를 이용한 heap으로 풀이
+	public static int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> heap = new PriorityQueue<Integer>();
 
-			// 2개 이상 존재할 경우 둘중 하나가 K보다 작을 경우
-			if (arr.get(0) < K) {
+        for (int aScoville : scoville) {
+            heap.add(aScoville);
+        }
+        
+        while (heap.peek() <= K) {
+            if (heap.size() == 1) {
+                return -1;
+            }
+            int a = heap.remove();
+            int b = heap.remove();
 
-				arr.add(0, (arr.remove(0) + (arr.remove(0) * 2)));
-				for (int i = 0; i < arr.size(); i++) {
-					System.out.print(arr.get(i) + " ");
-				}
-				System.out.println("size : " + arr.size());
-
-				answer++;
-			} else
-				return answer;
-		}
-	}
+            heap.add(a + (b * 2));
+            answer ++;
+        }
+        return answer;
+    }
 }
